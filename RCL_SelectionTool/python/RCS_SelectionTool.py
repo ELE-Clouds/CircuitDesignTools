9fed669d1854bcfdcf2647b18bd6519d44d7a482
hello_world <gt_china_bird@126.com>
2016-12-09 16:45:37 +0800

    Update RCS_SelectionTool.py

--- RCL_SelectionTool/python/RCS_SelectionTool.py
+++ RCL_SelectionTool/python/RCS_SelectionTool.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 import math
-N={'E6':6,'E12':12}
+N={'E6':6,'E12':12,'E24':24,'E48':48,'E96':96,'E192':192}
+Ni=['E6','E12','E24','E48','E96','E192']
+'''for E in N:
+    for di = 0 to 5:
+        for i in Ni:
+            val = di * 10 i/N[E] '''
